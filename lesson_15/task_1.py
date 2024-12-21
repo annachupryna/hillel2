d53a5b6ec660ad206b47fa16730dd2bbbb937c96
Anna Chupryna <anna.chupryna@cashberry.com.ua>
2024-12-21 12:38:20 +0100

    Changes added

--- lesson_15/task_1.py
+++ lesson_15/task_1.py
@@ -8,18 +8,19 @@
 Значення сторони сторона_а повинно бути більше 0.
 Кути кут_а та кут_б повинні задовольняти умову: кут_а + кут_б = 180
 Протилежні кути ромба завжди рівні, тому при заданому значенні кут_а, значення кут_б обчислюється автоматично.
-Для встановлення значень атрибутів використовуйте метод __setattr__."""
+Для встановлення значень атрибутів використовуйте метод __setattr__.
+"""
 
 
 class Rhombus:
     def __init__(self, side_a, angle_a):
         self.side_a = side_a
         self.angle_a = angle_a
-        self.angle_b = self.calculate_side_b()
+        self._angle_b = self.calculate_angle_b()  # роблю кут б умовно недостпуним для змін
 
-    def calculate_side_b(self):
-        side_b = 180 - self.side_a
-        return side_b
+    def calculate_angle_b(self):
+        angle_b = 180 - self.angle_a
+        return angle_b
 
     def __setattr__(self, key, value):
         if key == 'side_a':
@@ -27,13 +28,16 @@ class Rhombus:
                 raise AttributeError(f'Value for {key} should be 0 < but entered {value}.')
             super().__setattr__(key, value)
         elif key == 'angle_a':
-            if value <= 0:
+            if not 0 < value < 180:
                 raise AttributeError(f'Value for {key} should be 0 < but entered {value}.')
             super().__setattr__(key, value)
         else:
             super().__setattr__(key, value)
 
+    @property
+    def angle_b(self):  # геттер для кута б, при виклику проперті кута б буде спрацьовувати ця функція
+        return self._angle_b
 
-my_romb = Rhombus(3, 3)
 
+my_romb = Rhombus(3, 66)
 print(my_romb.side_a, my_romb.angle_a, my_romb.angle_b)
